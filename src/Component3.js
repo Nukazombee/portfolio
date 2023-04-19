36ae07d31daf2a6a2e16db71670e39fd9b9f6f98
Denis Kurdiukov <den.pixxxel@gmail.com>
2023-04-19 20:05:26 +1200

    v1.3 nicer

--- src/Component3.js
+++ src/Component3.js
@@ -5,68 +5,68 @@ export default function Component3() {
     <div>
       <div
         className="grid w-[90%] lg:w-[1000px] mx-auto mt-5 grid-cols-1 lg:grid-cols-2 lg:auto-rows-max lg:h-[1940px] auto-w
-       lg:gap-4 2xl:grid-cols-[330px_minmax(400px,_1fr)_660px] 2xl:w-[1500px] 2xl:h-full 2xl:grid-rows-[315px_335px_279px]"
+       lg:gap-4 2xl:grid-cols-[450px_minmax(450px,_1fr)_460px] 2xl:w-[1500px] 2xl:h-full 2xl:grid-rows-[260px_260px_410px]"
       >
         <div
-          className="bg-color11 shadow-xl p-4 pb-0 m-2 lg:m-0 rounded-3xl mx-auto w-[420px] md:w-[480px] lg:col-start-1 lg:col-end-3 lg:w-auto flex flex-col
-            items-center"
+          className="bg-color11 shadow-xl p-4 pb-0 m-2 lg:m-0 rounded-3xl mx-auto w-[420px] md:w-[480px] lg:col-start-1 lg:col-end-3 lg:w-full flex flex-col
+            items-center 2xl:order-1 2xl:col-start-1 2xl:col-end-3 2xl:row-start-1 2xl:row-end-4 2xl:flex-row 2xl:flex-wrap 2xl:justify-evenly 2xl:items-start"
         >
-          <h2 className="text-3xl font-extrabold text-white">WEB DEV</h2>
-          <div className=" p-4 m-2 mb-10 mx-auto w-full flex flex-col items-start">
-            <div className="bg-gray-700 flex justify-between items-center h-[240px] lg:h-[480px] w-full mx-auto ">
+          <h2 className="text-3xl font-extrabold text-white 2xl:w-full">WEB DEV</h2>
+          <div className=" p-4 m-2 mb-10 mx-auto w-full flex flex-col items-start 2xl:w-[45%] 2xl:mx-2 2xl:p-0 2xl:mb-2">
+            <div className="flex justify-between items-center h-[240px] lg:h-[480px] 2xl:h-[280px] w-full mx-auto ">
               <img
                 src="../photos/img1.jpg"
-                className="h-full w-full object-cover"
+                className="h-full w-full object-cover 2xl:rounded-2xl"
               />
             </div>
-            <h3 className="mx-auto m-2 text-[22px] font-bold text-white">
+            <h3 className="mx-auto m-2 text-[22px] font-bold text-white 2xl:font-semibold 2xl:mt-1 2xl:mx-0 2xl:ml-2 2xl:mb-1">
               FAhafhha ahfajjafjk
             </h3>
-            <p className="text-white">
+            <p className="text-white 2xl:text-left 2xl:ml-2 2xl:mt-0">
               Fafasgsad about algfk and slg;dsgsdl;gl using klasklfkl and wihtok
               sdg klsdgkl; lsdg. KGSKdskg. sdgkkdsg
             </p>
-            <div className="flex flex-wrap mt-4 lg:w-full lg:justify-center">
+            <div className="flex flex-wrap mt-4 lg:w-full lg:justify-center 2xl:justify-start 2xl:ml-1 2xl:mt-0">
               <p className="bg-color9 px-[8px] m-[4px]">HTML</p>
               <p className="bg-color10 px-[8px] m-[4px]">CSS</p>
               <p className="bg-color13 px-[8px] m-[4px]">TAILWIND</p>
             </div>
           </div>
-          <div className=" p-4 m-2 mb-10 mx-auto w-full flex flex-col items-start">
-            <div className="bg-gray-700 flex justify-between items-center h-[240px] lg:h-[480px] w-full mx-auto ">
+          <div className=" p-4 m-2 mb-10 mx-auto w-full flex flex-col items-start 2xl:w-[45%] 2xl:mx-2 2xl:p-0 2xl:mb-2">
+            <div className=" flex justify-between items-center h-[240px] lg:h-[480px] 2xl:h-[280px] w-full mx-auto ">
               <img
                 src="../photos/img2.jpg"
-                className="h-full w-full object-cover"
+                className="h-full w-full object-cover 2xl:rounded-2xl"
               />
             </div>
-            <h3 className="mx-auto m-2 text-[22px] font-bold text-white">
+            <h3 className="mx-auto m-2 text-[22px] font-bold text-white 2xl:font-semibold 2xl:mt-1 2xl:mx-0 2xl:ml-2 2xl:mb-1">
               FAhafhha ahfajjafjk
             </h3>
-            <p className="text-white">
+            <p className="text-white 2xl:text-left 2xl:ml-2 2xl:mt-0">
               Fafasgsad about algfk and slg;dsgsdl;gl using klasklfkl and wihtok
               sdg klsdgkl; lsdg. KGSKdskg. sdgkkdsg
             </p>
-            <div className="flex flex-wrap mt-4 lg:w-full lg:justify-center">
+            <div className="flex flex-wrap mt-4 lg:w-full lg:justify-center 2xl:justify-start 2xl:ml-1 2xl:mt-0">
               <p className="bg-color9 px-[8px] m-[4px]">HTML</p>
               <p className="bg-color10 px-[8px] m-[4px]">CSS</p>
               <p className="bg-color13 px-[8px] m-[4px]">TAILWIND</p>
             </div>
           </div>
-          <div className=" p-4 m-2 mb-10  mx-auto w-full flex flex-col items-start">
-            <div className="bg-gray-700 flex justify-between items-center h-[240px] lg:h-[480px] w-full mx-auto ">
+          <div className=" p-4 m-2 mb-10 mx-auto w-full flex flex-col items-start 2xl:w-[45%] 2xl:mx-2 2xl:p-0 2xl:mb-2">
+            <div className=" flex justify-between items-center h-[240px] lg:h-[480px] 2xl:h-[280px] w-full mx-auto ">
               <img
                 src="../photos/img3.jpg"
-                className="h-full w-full object-cover"
+                className="h-full w-full object-cover 2xl:rounded-2xl"
               />
             </div>
-            <h3 className="mx-auto m-2 text-[22px] font-bold text-white">
+            <h3 className="mx-auto m-2 text-[22px] font-bold text-white 2xl:font-semibold 2xl:mt-1 2xl:mx-0 2xl:ml-2 2xl:mb-1">
               FAhafhha ahfajjafjk
             </h3>
-            <p className="text-white">
+            <p className="text-white 2xl:text-left 2xl:ml-2 2xl:mt-0">
               Fafasgsad about algfk and slg;dsgsdl;gl using klasklfkl and wihtok
               sdg klsdgkl; lsdg. KGSKdskg. sdgkkdsg
             </p>
-            <div className="flex flex-wrap mt-4 lg:w-full lg:justify-center">
+            <div className="flex flex-wrap mt-4 lg:w-full lg:justify-center 2xl:justify-start 2xl:ml-1 2xl:mt-0">
               <p className="bg-color9 px-[8px] m-[4px]">HTML</p>
               <p className="bg-color10 px-[8px] m-[4px]">CSS</p>
               <p className="bg-color13 px-[8px] m-[4px]">TAILWIND</p>
@@ -74,59 +74,69 @@ export default function Component3() {
           </div>
         </div>
         <div
-          className="bg-white p-4 m-2 shadow-xl rounded-3xl mx-auto w-[420px] 2xl:h-full
-         md:w-[480px] lg:order-1 lg:col-start-1 lg:m-0 lg:col-end-2 lg:w-full lg:h-full 2xl:order-5 2xl:col-start-1 2xl:col-end-2 2xl:flex 2xl:items-center"
+          className="bg-white p-4 m-2 shadow-xl rounded-3xl mx-auto w-[420px] 
+         md:w-[480px] lg:order-1 lg:col-start-1 lg:m-0 lg:col-end-2 lg:w-full lg:h-full 2xl:order-2 2xl:col-start-3 2xl:col-end-4 2xl:row-start-1
+         2xl:row-end-3 2xl:flex 2xl:items-center 2xl:flex-col"
         >
-          <h2 className="text-3xl font-extrabold text-color11">PERSONAL</h2>
-          <div className="flex justify-center items-center 2xl:flex-wrap 2xl:justify-evenly 2xl:items-center">
-            <div>
+          <h2 className="text-3xl font-extrabold mb-6 text-color11 2xl:mb-3">PERSONAL</h2>
+          <div className="flex justify-center items-center lg:flex-col 2xl:flex-wrap 2xl:justify-evenly 2xl:items-center">
+            <div className="flex justify-between items-center lg:bg-color13 lg:mb-4  rounded-2xl">
               <img className="scale-75 " src="../photos/ic1a.png" />
-              <p>A ahfjasjkj about lasfl;s and sgldsgosdgok using ksdgsdlg;ls;dh and lgsdl;.</p>
+              <p className="hidden lg:inline-block p-2">A ahfjasjkj about lasfl;s and sgldsgosdgok using ksdgsdlg;ls;dh and lgsdl;.</p>
             </div>
-            <img className="scale-75 " src="../photos/ic2a.png" />
-            <img className="scale-75 " src="../photos/ic3a.png" />
-            <img className="scale-75 " src="../photos/ic4a.png" />
+            <div className="flex justify-between items-center lg:bg-color13 lg:mb-4  rounded-2xl">
+              <img className="scale-75 " src="../photos/ic2a.png" />
+              <p className="hidden lg:inline-block p-2">A ahfjasjkj about lasfl;s and sgldsgosdgok using ksdgsdlg;ls;dh and lgsdl;.</p>
+            </div>
+            <div className="flex justify-between items-center lg:bg-color13 lg:mb-4  rounded-2xl">
+              <img className="scale-75 " src="../photos/ic3a.png" />
+              <p className="hidden lg:inline-block p-2">A ahfjasjkj about lasfl;s and sgldsgosdgok using ksdgsdlg;ls;dh and lgsdl;.</p>
+            </div>
+            <div className="flex justify-between items-center lg:bg-color13 lg:mb-4  rounded-2xl">
+              <img className="scale-75 " src="../photos/ic4a.png" />
+              <p className="hidden lg:inline-block p-2">A ahfjasjkj about lasfl;s and sgldsgosdgok using ksdgsdlg;ls;dh and lgsdl;.</p>
+            </div>
           </div>
         </div>
         <div
-          className="bg-color11 shadow-xl p-4 pb-0 m-2 lg:m-0 rounded-3xl mx-auto w-[420px] md:w-[480px] flex flex-col
-            items-center lg:order-2"
+          className="bg-color11 shadow-xl p-4 pb-0 m-2 lg:m-0 rounded-3xl mx-auto w-[420px] md:w-[480px] lg:w-full flex flex-col
+            items-center lg:order-2 2xl:order-3 2xl:row-start-3 2xl:row-end-4 2xl:col-start-3 2xl:col-end-4 2xl:overflow-y-auto overflow-x-hidden"
         >
-          <h2 className="text-3xl font-extrabold text-white mb-6">BLOG</h2>
-          <div className="bg-white bg-opacity-20 m-1 mt-0 mb-3 p-4 pt-1 shadow-lg lg:w-[450px] 
-          rounded-2xl 2xl:w-[290px]  2xl:mb-0 2xl:flex-grow  2xl:p-2 2xl:h-[165px] ">
-            <date className="text-white">24 April</date>
+          <h2 className="text-3xl font-extrabold text-white mb-6 2xl:mb-3">BLOG</h2>
+          <div className="bg-white bg-opacity-20 m-1 mt-0 mb-3 p-4 pt-1 shadow-lg lg:w-full 
+          rounded-2xl 2xl:w-full  2xl:mb-6  2xl:p-2 2xl:min-h-fit 2xl:flex 2xl:flex-col">
+            <date className="text-white 2xl:text-left">24 April</date>
 
-            <h3 className="mx-auto m-2 text-[22px] font-bold text-white">
+            <h3 className="mx-auto m-2 text-[22px] font-bold text-white 2xl:mx-0 2xl:text-left 2xl:font-semibold">
               FAhafhha ahfajjafjk
             </h3>
-            <p className="text-white text-left">
+            <p className="text-white text-left 2xl:mb-5">
               Fafasgsad about algfk and slg;dsgsdl;gl using klasklfkl and wihtok
-              sdg klsdgkl; lsdg. KGSKdskg. sdgkkdsg
+              sdg klsdgkl; lsdg. KGSKdskg. sdgkkdsgadwawdawdawdawdawd wad awd dw
             </p>
           </div>
-          <div className="bg-white bg-opacity-20 m-1 mt-0 mb-3 p-4 pt-1 shadow-lg lg:w-[450px] 
-          rounded-2xl 2xl:w-[290px]  2xl:mb-0 2xl:flex-grow  2xl:p-2 2xl:h-[165px] ">
-            <date className="text-white">24 April</date>
+          <div className="bg-white bg-opacity-20 m-1 mt-0 mb-3 p-4 pt-1 shadow-lg lg:w-full 
+          rounded-2xl 2xl:w-full  2xl:mb-6  2xl:p-2 2xl:min-h-fit 2xl:flex 2xl:flex-col">
+            <date className="text-white 2xl:text-left">24 April</date>
 
-            <h3 className="mx-auto m-2 text-[22px] font-bold text-white">
+            <h3 className="mx-auto m-2 text-[22px] font-bold text-white 2xl:mx-0 2xl:text-left 2xl:font-semibold">
               FAhafhha ahfajjafjk
             </h3>
-            <p className="text-white text-left">
+            <p className="text-white text-left 2xl:mb-5">
               Fafasgsad about algfk and slg;dsgsdl;gl using klasklfkl and wihtok
-              sdg klsdgkl; lsdg. KGSKdskg. sdgkkdsg
+              sdg klsdgkl; lsdg. KGSKdskg. sdgkkdsgadwawdawdawdawdawd wad awd dw
             </p>
           </div>
-          <div className="bg-white bg-opacity-20 m-1 mt-0 mb-3 p-4 pt-1 shadow-lg lg:w-[450px] 
-          rounded-2xl 2xl:w-[290px]  2xl:mb-0 2xl:flex-grow  2xl:p-2 2xl:h-[165px] ">
-            <date className="text-white">24 April</date>
+          <div className="bg-white bg-opacity-20 m-1 mt-0 mb-3 p-4 pt-1 shadow-lg lg:w-full 
+          rounded-2xl 2xl:w-full  2xl:mb-6  2xl:p-2 2xl:min-h-fit 2xl:flex 2xl:flex-col">
+            <date className="text-white 2xl:text-left">24 April</date>
 
-            <h3 className="mx-auto m-2 text-[22px] font-bold text-white">
+            <h3 className="mx-auto m-2 text-[22px] font-bold text-white 2xl:mx-0 2xl:text-left 2xl:font-semibold">
               FAhafhha ahfajjafjk
             </h3>
-            <p className="text-white text-left">
+            <p className="text-white text-left 2xl:mb-5">
               Fafasgsad about algfk and slg;dsgsdl;gl using klasklfkl and wihtok
-              sdg klsdgkl; lsdg. KGSKdskg. sdgkkdsg
+              sdg klsdgkl; lsdg. KGSKdskg. sdgkkdsgadwawdawdawdawdawd wad awd dw
             </p>
           </div>
         </div>
